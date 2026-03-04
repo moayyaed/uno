d02a7d2ee8cca70f9ececf37b343ea72761d2d1d
moayyaed <13677197+moayyaed@users.noreply.github.com>
2026-03-04 01:22:16 +0000

    chore: Update external docs commit hashes

--- doc/import_external_docs.ps1
+++ doc/import_external_docs.ps1
@@ -18,7 +18,7 @@ $external_docs = @{
     "figma-docs"         = @{ ref="842a2792282b88586a337381b2b3786e779973b4" }  #latest main commit
     "uno.resizetizer"    = @{ ref="afba34d65a8077e8c29e78c5fda416ab4928d8df" }  #latest main commit
     "uno.uitest"         = @{ ref="94d027295b779e28064aebf99aeaee2b393ad558" }  #latest master commit
-    "uno.extensions"     = @{ ref="932844c77b5eaf86144e33d164fac63db63dee89" }  #latest main commit
+    "uno.extensions"     = @{ ref="7f8c24f629fb886cb9c9fb9cbf6312b1febc138d" }  #latest main commit
     "workshops"          = @{ ref="3515c29e03dea36cf2206d797d1bf9f8620370e3" }  #latest master commit
     "uno.samples"        = @{ ref="1d9ea60a7aec335e1d034446c631b93f605f06b8" }  #latest master commit
     "uno.chefs"          = @{ ref="06f4f8042595473557f00cdfa622788273d3a131" }  #latest main commit
